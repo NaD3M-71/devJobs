831fc9327174267e3b7fe8f63c8accfc40d81cb4
Giuliano Scaglioni <scaglioni96@gmail.com>
2024-04-02 19:54:49 -0300

    hotfix port

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.use((error,req,res,next)=>{
 
 // Dejar que Heroku nos asigne el puerto
 const host = '0.0.0.0';
-const puerto = process.env.PORT
+const port = process.env.PORT || 5000
 
 app.listen(port, host, ()=>{
     console.log('El servidor esta funcionando');
